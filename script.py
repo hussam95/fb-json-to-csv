7ab2d149dcb3a28c713facaf9479c2bcc0be409a
hussam95 <77602201+hussam95@users.noreply.github.com>
2022-02-14 18:46:26 +0500

    Add files via upload

--- script.py
+++ script.py
@@ -1,11 +1,22 @@
+
+"""Input --The script uses recursion to parse FB's JSON dump.
+Output --The script spits out single csv per JSON.
+root --The directory's path containing all the folders coming from FB; dtype=string. 
+output_loc --The path of desired location to store csvs; dtype=string.
+"""
+
 import os
 import pandas as pd
 import json
-
-            
+   
+#Input dir path containing all folders coming from FB
 root = "/home/nouman/Desktop/fb_parse/facebook-100007711683137/"
 folders = os.listdir(root)
 
+# Output dir that will contain csvs
+os.makedirs("/home/nouman/Desktop/fb_parse/"+"parsed_csvs",exist_ok=True)
+
+# Let's recursively parse each JSON present in each folder 
 for folder in folders:
     directory = folder
     path= os.path.join(root,directory)
@@ -48,20 +59,18 @@ for folder in folders:
                 elif type(json_object) is list:
                     for item in json_object:
                         parse_json_recursively(item)
-
+                #p. loc. exception list not having dic but a list
+            
             parse_json_recursively(developer)
 
-            pd.set_option('display.max_rows',None)
             df=pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in dic.items() ]))
+            
             #df=df[~(df.duplicated('timestamp'))].reset_index(drop=True)
             #df.dropna(axis=1,thresh=20)
-            df.to_csv(root+csv_file_name+'.csv')   
+            
+            output_loc= "/home/nouman/Desktop/fb_parse/parsed_csvs/"+file_name+'.csv'  
+            df.to_csv(output_loc)   
             #print(df)
 
-        
-            
 
-            
-            
-            
-             
+print(__doc__)        
